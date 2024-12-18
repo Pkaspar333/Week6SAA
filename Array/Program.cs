cf2fce0a56ed3c6573808394d25373ed54389e75
Kaspar P <kaspar.parve@tktk.ee>
2024-12-18 18:03:05 +0200

    week 6

---
+++ Array/Program.cs
@@ -0,0 +1,24 @@
+﻿string[] fruitBasket = { "apples", "bananas", "oranges" };
+
+Console.WriteLine($"There are {fruitBasket.Length} fruit in your basket.");
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
+
+fruitBasket[0] = "pine-apple";
+fruitBasket[1] = "peaches";
+
+//Console.WriteLine(fruitBasket[0]);
+//Console.WriteLine(fruitBasket[1]);
+//Console.WriteLine(fruitBasket[2]);
+
+for (int i = 0; i < fruitBasket.Length; i++)
+{
+    Console.WriteLine(fruitBasket[i]);
+}
---
+++ Week6SAA/Program.cs
@@ -0,0 +1,28 @@
+﻿//Console.WriteLine("Enter your first name:");
+//string userName  = Console.ReadLine().Trim();
+string userName = "Dark Vader";
+
+Console.WriteLine(userName.Length);
+Console.WriteLine($"The last letter in your name is {userName[userName.Length - 1]}.");
+
+//Console.WriteLine(userName[0]);
+//Console.WriteLine(userName[1]);
+//Console.WriteLine(userName[2]);
+//Console.WriteLine(userName[3]);
+//Console.WriteLine(userName[4]);
+
+//int counter = 0;
+
+//for (int i = 0; i < userName.Length; i++)
+//{
+//    if (userName[i] == a)
+//    { 
+//        counter++; //counter = counter + 1
+//    }
+//}
+
+//Console.WriteLine($"Your first name has {counter} a-letters.");
+
+string updatedUserName = userName.Replace('a', '4').Replace('e', '3');
+
+Console.WriteLine(updatedUserName);
